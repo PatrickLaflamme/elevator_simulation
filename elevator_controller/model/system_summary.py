08ba1895bbc43da500dd0cdbbd3939b6f93dcc91
patricklaflamme <patrickj.laflamme@gmail.com>
2023-09-30 12:37:53 -0400

    introduce logic to track the summary statistics

--- elevator_controller/model/system_summary.py
+++ elevator_controller/model/system_summary.py
@@ -1,10 +1,25 @@
 import sys
+from typing import Dict, Union
 
-from pydantic import BaseModel
 
-
-class SystemSummary(BaseModel):
-    min: int = sys.maxsize
-    max: int = -1
+class SystemSummary:
+    min_value: int = sys.maxsize
+    max_value: int = -1
     sum: int = 0
     n: int = 0
+
+    def add(self, time: int):
+        self.min_value = min(self.min_value, time)
+        self.max_value = max(self.max_value, time)
+        self.sum += time
+        self.n += 1
+
+    def mean(self) -> float:
+        return self.sum / self.n
+
+    def __dict__(self) -> Dict[str, Union[int, float]]:
+        return {
+            "min": self.min_value,
+            "max": self.max_value,
+            "mean": self.mean()
+        }
