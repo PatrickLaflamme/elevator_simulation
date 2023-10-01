d67256587e551f46d8537d58d8d72cbdbac7a1b5
patricklaflamme <patrickj.laflamme@gmail.com>
2023-10-01 12:37:16 -0400

    refactor system summary test to be more succinct.

--- test/model/test_system_summary.py
+++ test/model/test_system_summary.py
@@ -2,13 +2,15 @@ from elevator_controller.model.system_summary import SystemSummary
 
 
 def test_summary_stats():
+    # given
     summary = SystemSummary()
+
+    # when
     for i in range(10):
         summary.add(i)
-    assert(summary.max_value == 9)
-    assert(summary.min_value == 0)
-    assert(summary.mean() == 4.5)
     results = summary.__dict__()
+
+    # then
     assert(results.get('max') == 9)
     assert(results.get('min') == 0)
     assert(results.get('mean') == 4.5)
