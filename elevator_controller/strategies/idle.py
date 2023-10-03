8da099d9f3fb50ea9d14726f381e6acaa116947c
patricklaflamme <patrickj.laflamme@gmail.com>
2023-10-02 20:43:04 -0400

    update idle strategy logic to support the elevator refactor.

--- elevator_controller/strategies/idle.py
+++ elevator_controller/strategies/idle.py
@@ -11,15 +11,15 @@ class ElevatorIdleStrategy(Protocol):
 class MiddleFloorIdleStrategy:
     def position_idle_elevators(self, elevators: List[Elevator]):
         for elevator in elevators:
-            middle_floor = elevator.top_floor // 2
+            middle_floor = elevator.num_floors // 2
             elevator.idle_target = middle_floor
 
 
 class EqualSpreadIdleStrategy:
     def position_idle_elevators(self, elevators: List[Elevator]):
         assert len(elevators) > 0
-        assert len(set([e.top_floor for e in elevators])) == 1
-        top_floor = elevators[0].top_floor
+        assert len(set([e.num_floors for e in elevators])) == 1
+        top_floor = elevators[0].num_floors
         idle_elevators = [e for e in elevators if e.is_empty()]
         for i in range(len(idle_elevators)):
             elevator = idle_elevators[i]
